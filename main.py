8ee092bd797c04f6c572a03d5e8d0961357579ea
Timo Bergen <timo@bergen-it.net>
2023-09-19 23:43:24 +0200

    feat: add help command

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-import discord
 import os
+import discord
+from discord import app_commands as commands
 
 intents = discord.Intents.default()
 intents.voice_states = True
 
 client = discord.Client(intents=intents)
+tree = commands.CommandTree(client)
 
 DISCORD_TOKEN = os.environ['LOGBOT_TOKEN']
 DISCORD_CHANNEL = int(os.environ['LOGBOT_CHANNEL'])
 
+@tree.command(name = "help", description = "Show this help message.")
+async def first_command(interaction):
+    await interaction.response.send_message("This bot actually has no commands, please just setup the env-Variables for the DISCORD_CHANNEL & DISCORD_TOKEN!")
+
 @client.event
 async def on_ready():
+    await tree.sync()
     print('Logged in as {0.user}'.format(client))
 
 @client.event
