ea5c7a06c14051d2bbe9c29156e960c31a997d73
Timo Bergen <mail@timobergen.de>
2025-01-03 18:30:23 +0100

    chore: added ping pong command for badge

--- go/main.go
+++ go/main.go
@@ -9,6 +9,8 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+var botID string
+
 func main() {
 	discordToken := os.Getenv("LOGBOT_TOKEN")
 	if discordToken == "" {
@@ -27,15 +29,19 @@ func main() {
 		return
 	}
 
-	// Add event handlers
-	dg.AddHandler(voiceStateUpdate)
+	// Get bot user ID for later checks
 	dg.AddHandlerOnce(func(s *discordgo.Session, ready *discordgo.Ready) {
-		// Send a message to the log channel when the bot is ready
+		botID = s.State.User.ID
+		// Send startup message
 		_, err := s.ChannelMessageSend(logChannel, "Bot is up and running!")
 		if err != nil {
 			fmt.Println("Error! Sending startup message:", err)
 		}
 	})
+
+	// Add handlers for events
+	dg.AddHandler(voiceStateUpdate)
+	dg.AddHandler(handleInteraction)
 
 	err = dg.Open()
 	if err != nil {
@@ -43,13 +49,55 @@ func main() {
 		return
 	}
 
+	// Register application commands
+	registerCommands(dg)
+
 	fmt.Println("Success! Bot is now running.")
 
+	// Graceful shutdown
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
+	// Clean up commands on shutdown
+	dg.ApplicationCommandBulkOverwrite(dg.State.User.ID, "", nil)
 	dg.Close()
+}
+
+func registerCommands(s *discordgo.Session) {
+	commands := []*discordgo.ApplicationCommand{
+		{
+			Name:        "ping",
+			Description: "Responds with Pong!",
+		},
+	}
+
+	// Register commands globally
+	for _, cmd := range commands {
+		_, err := s.ApplicationCommandCreate(s.State.User.ID, "", cmd)
+		if err != nil {
+			fmt.Printf("Error creating command %s: %v\n", cmd.Name, err)
+		} else {
+			fmt.Printf("Command %s registered.\n", cmd.Name)
+		}
+	}
+}
+
+func handleInteraction(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	if i.Type == discordgo.InteractionApplicationCommand {
+		switch i.ApplicationCommandData().Name {
+		case "ping":
+			err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Pong!",
+				},
+			})
+			if err != nil {
+				fmt.Println("Error responding to /ping command:", err)
+			}
+		}
+	}
 }
 
 func voiceStateUpdate(s *discordgo.Session, vsu *discordgo.VoiceStateUpdate) {
